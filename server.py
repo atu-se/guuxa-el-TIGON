7635a8e046104691f7b0793bb87fbf4e8244d057
el-TIGON <71264570+el-TIGON@users.noreply.github.com>
2021-09-22 13:45:14 +0100

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn,addr = s.accept()
         with conn:
             print('connection opened by', addr)
+            # you may wish to use full_message
+            # remember that it is a bytes array and if you
+            # compare it to a string, you should convert
+            # the string to a bytearray:  b'mystring'
             full_message = bytearray()
             data = conn.recv(BUFFER_SIZE)
             full_message += data
